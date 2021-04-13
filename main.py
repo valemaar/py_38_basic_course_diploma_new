ea9707f7dbba410ead1371ba68049e132239b54b
Marat Valeev <valeeva.la.1986@gmail.com>
2021-04-13 16:06:55 +0500

    1. Edit the ability to select the number of photos to download. 2. Added the ability to enter user_id or screen_name.

--- main.py
+++ main.py
@@ -1,12 +1,8 @@
 import json
-import sys
 
 import requests
 
 from datetime import datetime
-
-import time  # при установке модуля выдает ошибку
-# "ERROR: Could not find a version that satisfies the requirement time (from versions: none)"
 
 
 class VkUser:
@@ -14,6 +10,7 @@ class VkUser:
     url = 'https://api.vk.com/method/'
 
     def __init__(self, version='5.130'):
+        # Инициализация экземпляра класса
         self.token = '958eb5d439726565e9333aa30e50e0f937ee432e927f0dbd541c541887d919a7c56f95c04217915c32008'
         self.version = version
         self.params = {
@@ -22,21 +19,38 @@ class VkUser:
         }
 
     def get_numbers_of_photo(self):
-        count_photo = input(f'Сколько фотографий Вы хотите скачать (по-умолчанию 5 штук)?\n')
-        try:
-            if count_photo == '':
-                count_photo = 5
-                print('Будет загружено 5 фотографий.')
-            elif int(count_photo) in range(1001):
+        # Метод запрашивает у пользователя количество скачиваемых фотографий
+        while True:
+            try:
+                count_photo = input(f'Сколько фотографий Вы хотите скачать (по-умолчанию 5 штук, '
+                                        f'максимальное количество 1000 штук)?\n')
+                if count_photo == '':
+                    count_photo = 5
                 count_photo = int(count_photo)
-        except:
-            print('Ошибка! Неверный ввод!')
-            sys.exit()
+            except ValueError:
+                print(f'Ошибка! Неверный ввод!\n')
+            else:
+                break
         return count_photo
 
-    def get_photos(self, count_photo='5'):
+    def get_vk_user_id(self):
+        # метод принимает идентификатор (user_id) или короткое имя (screen_name) пользователя и возвращает
+        # его id для дальнейшей обработки
+        user_url = self.url + 'users.get'
+        user_id = input(f'Введите короткое имя или id пользователя социальной сети "ВКонтакте":\n')
+        user_params = {
+            'user_ids': user_id,
+        }
+        res = requests.get(user_url, params={**self.params, **user_params})
+        vk_user_id = res.json()['response'][0]['id']
+        return vk_user_id
+
+    def get_photos(self):
+        # метод сохраняет в json-файл служебную информацию о фото, для последующего использования этой
+        # информации во время загрузки фото в Яндекс.Диск. Также другой json-файл сохраняется на жесткий диск
+        # и содержит информацию о фото в требуемом по заданию виде
         photos_url = self.url + 'photos.get'
-        user_id = input(f'Введите id пользователя социальной сети "ВКонтакте":\n')
+        user_id = self.get_vk_user_id()
         count_photo = self.get_numbers_of_photo()
         photos_params = {
             'count': count_photo,
@@ -79,7 +93,7 @@ class VkUser:
                     temp_name = str(photo_likes) + '.jpg'
                     temp_dict['filename'] = temp_name
 
-                    # сохраняем информацию о фото в json-файл
+                    # сохраняем информацию о фото в json-файл на жестком диске в требуемом по заданию виде
                     for name in json_data_file:
                         if temp_name in name.values():
                             temp_dict['filename'] = str(photo_likes) + '_' + str(photo_date) + '.jpg'
@@ -91,6 +105,7 @@ class VkUser:
                     # заполняем json-файл для загрузки фото в Яндекс.Диск
                     temp_dict_upload = temp_dict.copy()
                     temp_dict_upload['url'] = size['url']
+                    temp_dict_upload['user_id'] = user_id
                     json_data_upload_yadisc.append(temp_dict_upload)
 
                 json.dump(json_data_file, f, ensure_ascii=False, indent=2)
@@ -103,6 +118,7 @@ class VkBackupPhotos:
         self.token = input(f'\nВведите токен пользователя Яндекс.Диск:\n')
 
     def get_headers(self):
+        # метод возвращает заголовки (headers) для http-запросов
         return {
             'Content-Type': 'application/json',
             'Authorization': 'OAuth {}'.format(self.token)
@@ -121,14 +137,16 @@ class VkBackupPhotos:
         # метод создает папку в Яндекс.Диск
         upload_url = 'https://cloud-api.yandex.net/v1/disk/resources'
         headers = self.get_headers()
-        folder_name = 'VK_Photos'
+        folder_name = 'New_photos'
         params = {'path': folder_name, 'overwrite': 'true'}
         response = requests.put(upload_url, headers=headers, params=params)
         response_info = requests.get(upload_url, headers=headers, params=params)
-        message = f'\nПапка "{folder_name}" создана в Вашем Яндекс.Диске.\n'
+        message = f'Папка "{folder_name}" создана в Вашем Яндекс.Диске.\n'
         return print(message), response.json(), response_info.json()
 
     def upload(self, source_json):
+        print(f'Найдено {len(source_json)} фотографий\n')
+
         # создаем папку VK_Photos в Яндекс.Диск
         self.create_folder_yadisc()
 
@@ -141,12 +159,12 @@ class VkBackupPhotos:
                     f.write(requests.get(photo['url']).content)
 
                 # путь к папке VK_Photos в Яндекс.Диск
-                yadisc_file_path = 'VK_Photos/' + str(photo['filename'])
+                yadisc_file_path = 'New_photos/' + str(photo['filename'])
 
                 # получаем ссылку для загрузки в Яндекс.Диск
                 href = self._get_upload_link(disk_file_path=yadisc_file_path).get('href', '')
 
-                # логирование процесса загрузки :)
+                # логирование процесса загрузки
                 print(f'Загружено фотографий ... {count} из {len(source_json)}')
 
                 # загружаем фото в Яндекс.Диск
